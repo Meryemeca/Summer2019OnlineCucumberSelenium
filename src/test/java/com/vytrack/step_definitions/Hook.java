be15337a1b24b34e7fc051f413dddf0d512b2226
Meryemeca <meryem.kus@gmail.com>
2019-12-17 15:46:53 -0600

    first commit

---
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -0,0 +1,25 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hook {
+
+    @Before
+    public void setup(){
+        System.out.println("This setup");
+    }
+
+    @After
+    public void teardown(Scenario scenario){
+
+        if(scenario.isFailed()){
+            System.out.println("Test failed!");
+        }
+        else {
+            System.out.println("Clean up!");
+            System.out.println("Test completed");
+        }
+    }
+}
